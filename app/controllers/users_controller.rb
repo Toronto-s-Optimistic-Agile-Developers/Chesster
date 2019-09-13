a66951eaddb26c5825fdfe82319a82ab63afd8bd
Stephen <stephenmelnick8@gmail.com>
2019-09-13 05:32:03 +0000

    user controller changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ before_action :authenticate_user!, only: [:new]
   end
 
   def create
-
+    current_user.users.create(user_params)
+    redirect_to root_path
   end
 
   def show
-
+    @user = User.find(params[:id])
   end 
 
 private
