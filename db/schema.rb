98a768cb58605ed91db9c993ca5b3d2860b7c65f
Stephen <stephenmelnick8@gmail.com>
2019-09-13 19:57:16 +0000

    fixed issue

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,15 @@ ActiveRecord::Schema.define(version: 2019_09_11_004254) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
