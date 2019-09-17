8e3c79327125970795490aa8b4e679271106e353
Stephen <stephenmelnick8@gmail.com>
2019-09-17 17:27:08 +0000

    removed users controller db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2019_09_14_225620) do
 
   create_table "games", force: :cascade do |t|
     t.string "username"
-    t.string "user_id"
     t.integer "player_id"
     t.integer "white_id"
     t.integer "black_id"
@@ -49,13 +48,12 @@ ActiveRecord::Schema.define(version: 2019_09_14_225620) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
