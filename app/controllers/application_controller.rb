a0c87205fb0fd41d5876f794b4680b1fd7c1632d
Wilson Horrell <wilsonhorrell@gmai.com>
2019-09-22 23:47:24 +0000

    Made changes to allow for creating games.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,7 @@ class ApplicationController < ActionController::Base
     redirect_to :login unless user_signed_in?
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  
 
   def user_signed_in?
     # converts current_user to a boolean by negating the negation
