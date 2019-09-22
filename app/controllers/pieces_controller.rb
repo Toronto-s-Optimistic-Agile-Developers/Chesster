310571e37ecb73627da27866e7929af1f6127efe
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-09-22 23:56:48 +0000

    Fixing .permit for params

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -15,7 +15,7 @@ class PiecesController < ApplicationController
   private
 
   def piece_params
-    params.require(:piece).permit(:x_coord, :y_coord, :game_id, :player_id, :type)
+    params.require(:piece).permit(:name, :color, :x_coord, :y_coord, :game_id, :player_id, :image, :type)
   end
 end
 
