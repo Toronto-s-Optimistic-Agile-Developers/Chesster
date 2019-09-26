be2031955e39a5c6926cad48d831e1ea15a2be75
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-09-26 22:48:52 +0000

    Adding tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
@@ -15,6 +13,15 @@ RSpec.describe GamesController, type: :controller do
       get :show
       expect(response).to have_http_status(:success)
     end
+    describe 'setup_board!' do
+      it 'should add 16 white and 16 black pieces to board' do
+        user = FactoryBot.create(:user)
+        sign_in user
+        game = FactoryBot.create(:game)
+        game.set_up_board!
+        expect(pieces.count).to eq(32)
+      end
+    end
   end
 
   describe 'game#new' do
@@ -28,7 +35,7 @@ RSpec.describe GamesController, type: :controller do
     it 'returns a success response' do
       user = FactoryBot.create(:user)
       sign_in user
-      game = game = FactoryBot.create(:game)
+      game = FactoryBot.create(:game)
       get :edit
       expect(response).to have_http_status(:success)
     end
@@ -44,7 +51,7 @@ RSpec.describe GamesController, type: :controller do
       end.to change(Game, :count).by(1)
     end
 
-    it 'redirects to the created game' do
+    it 'redirects to created game' do
       user = FactoryBot.create(:user)
       sign_in user
       game = game = FactoryBot.create(:game)
@@ -52,7 +59,7 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(Game.last)
     end
 
-    it "returns a success response (i.e. to display the 'new' template)" do
+    it "returns a success response (i.e. to display 'new' template)" do
       user = FactoryBot.create(:user)
       sign_in user
       game = game = FactoryBot.create(:game)
@@ -60,42 +67,253 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-end
 
-describe 'PUT #update' do
-  it 'updates the requested game' do
-    user = FactoryBot.create(:user)
-    sign_in user
-    game = game = FactoryBot.create(:game)
-    put :update
-    game.reload
-    skip('Add assertions for updated state')
+
+  describe 'PUT #update' do
+    it 'updates requested game' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = game = FactoryBot.create(:game)
+      put :update
+      game.reload
+      skip('Add assertions for updated state')
+    
+
+    it 'redirects to game' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = game = FactoryBot.create(:game)
+      put :update
+      expect(response).to redirect_to(game)
+    end
   end
 
-  it 'redirects to the game' do
-    user = FactoryBot.create(:user)
-    sign_in user
-    game = game = FactoryBot.create(:game)
-    put :update
-    expect(response).to redirect_to(game)
-  end
-end
+  describe 'DELETE #destroy' do
+    it 'destroys requested game' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = game = FactoryBot.create(:game)
+      expect do
+        delete :destroy
+      end.to change(Game, :count).by(-1)
+    end
 
-describe 'DELETE #destroy' do
-  it 'destroys the requested game' do
-    user = FactoryBot.create(:user)
-    sign_in user
-    game = game = FactoryBot.create(:game)
-    expect do
+    it 'redirects to games list' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = game = FactoryBot.create(:game)
       delete :destroy
-    end.to change(Game, :count).by(-1)
+      expect(response).to redirect_to(games_url)
+    end
   end
 
-  it 'redirects to the games list' do
-    user = FactoryBot.create(:user)
-    sign_in user
-    game = game = FactoryBot.create(:game)
-    delete :destroy
-    expect(response).to redirect_to(games_url)
+  describe 'set_up_board!' do
+    it 'Adds correct number of white Pawns to board' do
+        user = FactoryBot.create(:user)
+        sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 0, y_coord: 1).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 1, y_coord: 1).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 2, y_coord: 1).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 3, y_coord: 1).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 4, y_coord: 1).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 5, y_coord: 1).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 6, y_coord: 1).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 7, y_coord: 1).type).to eq 'Pawn'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds white Rook to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 0, y_coord: 0).type).to eq 'Rook'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds white Rook to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 7, y_coord: 0).type).to eq 'Rook'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds white Knight to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 1, y_coord: 0).type).to eq 'Knight'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds white Knight to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 6, y_coord: 0).type).to eq 'Knight'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds white Bishop to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 2, y_coord: 0).type).to eq 'Bishop'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds white Bishop to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 5, y_coord: 0).type).to eq 'Bishop'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds white King to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 4, y_coord: 0).type).to eq 'King'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds white Queen to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 3, y_coord: 0).type).to eq 'Queen'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds correct number of black Pawns to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 0, y_coord: 6).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 1, y_coord: 6).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 2, y_coord: 6).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 3, y_coord: 6).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 4, y_coord: 6).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 5, y_coord: 6).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 6, y_coord: 6).type).to eq 'Pawn'
+      expect(game.pieces.find_by(x_coord: 7, y_coord: 6).type).to eq 'Pawn'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds black Rook to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 0, y_coord: 7).type).to eq 'Rook'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds black Rook to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 7, y_coord: 7).type).to eq 'Rook'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds black Knight to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 1, y_coord: 7).type).to eq 'Knight'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds black Knight to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 6, y_coord: 7).type).to eq 'Knight'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds black Bishop to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 2, y_coord: 7).type).to eq 'Bishop'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds black Bishop to board' do
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 5, y_coord: 7).type).to eq 'Bishop'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds black King to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 4, y_coord: 7).type).to eq 'King'
+    end
+  end
+
+  describe 'set_up_board!' do
+    it 'Adds black Queen to board' do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = Game.create!
+      game.set_up_board!
+
+      expect(game.pieces.find_by(x_coord: 3, y_coord: 7).type).to eq 'Queen'
+    end
   end
 end
