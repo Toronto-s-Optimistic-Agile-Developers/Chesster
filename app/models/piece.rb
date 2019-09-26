0a75a8988c32a8e91e0d737c50bda3b209d87ad0
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-09-26 22:40:02 +0000

    attempt 1 at move_to! and piece test

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,6 +1,48 @@
 class Piece < ApplicationRecord
   belongs_to :game
   belongs_to :user, required: false
-
-
+  
+    def location(x,y)
+      x = @piece_params[:x_coord]
+      y = @piece_params[:y_coord]
+    end
+  
+    def color
+      color ? 'white' : 'black'
+    end
+  
+    def white?
+      white
+    end
+  
+    def black?
+      !white
+    end
+  
+    def removed?
+      if @piece.params[x_coord: nil, y_coord: nil]
+        @piece.captured = true
+      end
+  end
+  
+    def valid_move?
+    end
+  
+    def has_moved?
+      if @piece.initial_postion?
+        false
+      end
+    end
+    def move_to!(x, y)
+      rival_piece = game.pieces.find_by(position_x: x, position_y: y)
+      if rival_piece.present? && rival_piece.color != color
+        rival_piece.update_attributes(x_coord: nil, y_coord: nil)
+        update_attributes(x_coord: x, y_coord)
+      elsif rival_piece.present? == false
+        update_attributes(x_coord: x, y_coord: y)
+      else
+        flash[:danger] = "Move cannot be completed."
+      end
+    end
+  end
 end
