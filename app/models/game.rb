b6ac7ca398a8d414f1360f5b77a0488a2af2bc06
Adpbonar <50775855+Adpbonar@users.noreply.github.com>
2019-09-29 22:50:15 -0400

    Fixing game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Game < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :white_id, class_name: 'User', optional: true
   belongs_to :black_id, class_name: 'User', optional: true
 
@@ -16,19 +15,10 @@ class Game < ApplicationRecord
   def tile_taken?(x_path, y_path)
     pieces.where(x_coord: x_path, y_coord: y_path).first.present? 
   end 
-=======
-  belongs_to :white_player, class_name: 'User', optional: true
-  belongs_to :black_player, class_name: 'User', optional: true
-
-  has_many :pieces
-  has_many :users
-  
-  scope :available, -> { where(black_player: nil) }
 
   def unmatched_games
     Game.where(black_player: nil) && Game.where(white_player: !nil)
   end
->>>>>>> f1d37445f8ec89cf30881bc21942ee2a9fc4d320
   
   #validates :name, presence: true
   def set_up_board!
@@ -68,20 +58,15 @@ class Game < ApplicationRecord
       Bishop.create(game_id: id, color: "black", x_coord: x_coord, y_coord: 0, name: "Black_Bishop")
     end
 
-<<<<<<< HEAD
+
     #Kings
-=======
-    #King
->>>>>>> f1d37445f8ec89cf30881bc21942ee2a9fc4d320
+
     King.create(game_id: id, color: "white", x_coord: 4, y_coord: 7, name: "White_King")
 
     King.create(game_id: id, color: "black", x_coord: 4, y_coord: 0, name: "Black_King")
 
-<<<<<<< HEAD
     #Queens
-=======
-    #Queen
->>>>>>> f1d37445f8ec89cf30881bc21942ee2a9fc4d320
+
     Queen.create(game_id: id, color: "white", x_coord: 3, y_coord: 7, name: "White_Queen")
 
     Queen.create(game_id: id, color: "black", x_coord: 3, y_coord: 0, name: "Black_Queen")
