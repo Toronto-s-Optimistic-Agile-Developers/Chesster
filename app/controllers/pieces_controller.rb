5c91e0275ccf84c85c9d1e7ca028190298ae017c
Stephen <stephenmelnick8@gmail.com>
2019-09-30 21:26:45 +0000

    pieces are now urls

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,24 +1,38 @@
-before_action :find_piece
-def new
-  @piece = game.pieces.new
-end
+class PiecesController < ApplicationController
 
-def create
-  @piece = game.pieces.create(piece_params)
-  if @piece.valid?
-    redirect_to games_path(@game)
-  else
-    render :new, status: :unprocessable_entity
+  before_action :find_piece
+
+  def new
+    @piece = game.pieces.new
   end
-end
 
-private
-def find_piece
-  @piece = Piece.find(params[:id])
-  @color = @piece.color
-  @game = @piece.game
-end
-def piece_params
-  params.require(:piece).permit(:name, :color, :x_coord, :y_coord, :game_id, :player_id, :type)
-end
+  def create
+    @piece = game.pieces.create(piece_params)
+    if @piece.valid?
+      redirect_to games_path(@game)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @piece = Piece.find_by_id(params[:id])
+  end
+
+  def update
+    @pieces.update_attributes(type: params[:piece][:type])
+    redirect_to game_path(@games)
+  end
+
+  private
+
+  def find_piece
+    @piece = Piece.find(params[:id])
+    @color = @piece.color
+    @game = @piece.game
+  end
+
+  def piece_params
+    params.require(:piece).permit(:name, :color, :x_coord, :y_coord, :game_id, :player_id, :type)
+  end
 end
