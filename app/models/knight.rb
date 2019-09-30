418c37cb32e8d1e1174e2b36c7dd5321483db326
Mike Tracz <miketracz@hotmail.com>
2019-09-30 17:47:33 +0000

    knight model updated

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,9 +3,8 @@ class Knight < Piece
 		return y_coord > 5 ? "&#9816;" : "&#9822;"	
 	end
 
-  def legal_move?(x, y)
-    #move must be to new square
-    return false if x_coord == x || y_coord == y
+  def legal_move?(x_path, y_path)
+    return false if x_coord == x_path || y_coord == y_path
     return true if knight_moves?(x, y)
     false
     end 
