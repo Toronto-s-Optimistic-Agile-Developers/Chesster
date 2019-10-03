f048918e0608980c383d7c9b6d5c1f7fb3a4ee4e
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-10-03 01:54:52 +0000

    Getting a method

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,6 +1,13 @@
 class Pawn < Piece
   def unicode_symbol	
 		return y_coord > 5 ? "&#9817;" : "&#9823;"
-		
 	end
+
+	def legal_move?(x_path, y_path)
+	  y_dif = (y_path - y_coord).abs
+		(y_dif == 1)
+		if (x_coord + 1) && (y_coord + 1) || (x_coord - 1) && (y_coord - 1)
+			self.move_to! && != self.friendly_piece
+		end
+  end
 end
