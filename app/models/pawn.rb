4c0d5366ba3220b70be3d5083999cf5b7ec5161c
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-10-05 21:53:27 +0000

    Making a few adjustments

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -5,9 +5,7 @@ class Pawn < Piece
 
 	def legal_move?(x_path, y_path)
 	  y_dif = (y_path - y_coord)
-		(y_dif == 1)
 		x_dif = (y_path - x_coord)
-		(x_dif == 0)
 		if self.has_moved? == false
 			(x_dif == 0 && y_dif == 1) || (x_dif == 0 && y_dif == 2)
 		else
