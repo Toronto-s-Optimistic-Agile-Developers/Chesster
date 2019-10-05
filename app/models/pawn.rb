6d1a7372e7752a145b9348bc207ff6018a1ce04f
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-10-05 22:18:21 +0000

    Working on valid move for the rook

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -6,7 +6,7 @@ class Pawn < Piece
 	def legal_move?(x_path, y_path)
 	  y_dif = (y_path - y_coord)
 		x_dif = (y_path - x_coord)
-		if self.has_moved? == false
+		if self.has_moved? 
 			(x_dif == 0 && y_dif == 1) || (x_dif == 0 && y_dif == 2)
 		else
 			(x_dif == 0 && y_dif == 1)
