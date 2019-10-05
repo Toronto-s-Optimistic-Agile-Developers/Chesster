cde9c2c57b7261dedf0da6d18c9ad4953d0f2a29
Wilson Horrell <wilsonhorrell@gmai.com>
2019-10-05 23:49:17 +0000

    drag_and_drop

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,19 +1,8 @@
 class PiecesController < ApplicationController
 
-  before_action :find_piece
+  
 
-  def new
-    @piece = game.pieces.new
-  end
-
-  def create
-    @piece = game.pieces.create(piece_params)
-    if @piece.valid?
-      redirect_to games_path(@game)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  
 
   def show
     @piece = Piece.find_by_id(params[:id])
@@ -21,19 +10,6 @@ class PiecesController < ApplicationController
     @pieces = @game.pieces  
   end
 
-  def update
-   
-  end
-
-  private
-
-  def find_piece
-    @piece = Piece.find(params[:id])
-    @color = @piece.color
-    @game = @piece.game
-  end
-  def piece_params
-    params.require(:piece).permit(:name, :color, :x_coord, :y_coord, :game_id, :player_id, :type)
-  end
+  
 end
 
