c0810e78b2f5ef84d637bd957a6870aecfa27c60
Mike Tracz <miketracz@hotmail.com>
2019-10-07 22:41:57 +0000

    rook

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,5 +1,12 @@
 class Bishop < Piece
-  def unicode_symbol	
-		return y_coord > 5 ? "&#9815;" : "&#9821;"
-	end
+  def unicode_symbol  
+    return y_coord > 5 ? "&#9815;" : "&#9821;"
+  end
+
+  def legal_move?(x_path, y_path)
+    x_dif = (x_path - x_coord).abs
+    y_dif = (y_path - y_coord).abs
+    return false if is_obstructed?(x_path, y_path)
+    (x_dif >= 1 && y_dif >= 1) && diagonal_move?(x_dif, y_dif)
+  end
 end
