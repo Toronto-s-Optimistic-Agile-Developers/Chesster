0546c00b23bbca4f2dd41efff6bdaf0cf5d173da
Mike Tracz <miketracz@hotmail.com>
2019-10-07 15:59:24 +0000

    rook legal_move?

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -2,4 +2,11 @@ class Rook < Piece
   def unicode_symbol	
 		return y_coord > 5 ? "&#9814;" : "&#9820;"
 	end
+
+  def legal_move?(x_path, y_path)
+    x_dif = (x_path - x_coord).abs
+    y_dif = (y_path - y_coord).abs
+    return false if is_obstructed?(x_path, y_path)
+    (x_dif >= 1 && y_dif == 0) || (y_dif >= 1 && x_dif == 0)
+  end
 end
