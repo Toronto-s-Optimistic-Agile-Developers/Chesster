f6d0698259035d4e6eff5df898c7a98548f0eec8
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-10-22 23:10:59 +0000

    test

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -19,7 +19,11 @@ class PiecesController < ApplicationController
     x_path = piece_params[:x_coord].to_i
     y_path = piece_params[:y_coord].to_i
     new_rank =  piece_params[:promotion_type].to_s
-    if @piece.promotion? == true
+    if @piece.name == "Black_King"  && ((@piece.x_coord == 0) || (@piece.x_coord == 7)) && (@piece.y_coord == 0)
+      @piece.castle(x_path, y_path)
+    elsif @piece.name == "White_King" && ((@piece.x_coord == 0) || (@piece.x_coord == 7)) && (@piece.y_coord == 7)
+      @piece.castle(x_path, y_path)
+    elsif @piece.promotion? == true
       @piece.update(piece_params)
       @piece.pawn_promote(new_rank)
       redirect_to @game
@@ -52,6 +56,6 @@ class PiecesController < ApplicationController
   end
 
   def piece_params
-    params.require(:piece).permit(:name, :color, :x_coord, :y_coord, :game_id, :player_id, :captured, :initial_position?, :promotion?, :promotion_type)
+    params.require(:piece).permit(:id, :name, :color, :x_coord, :y_coord, :game_id, :player_id, :captured, :initial_position?, :promotion?, :promotion_type)
   end
 end
