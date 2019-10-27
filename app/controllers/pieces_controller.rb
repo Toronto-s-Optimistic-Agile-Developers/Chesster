0817b857c962b4d82023d4322dc02e1cf779d93a
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-10-27 22:45:43 +0000

    testing logic

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -18,7 +18,13 @@ class PiecesController < ApplicationController
     x_path = piece_params[:x_coord].to_i
     y_path = piece_params[:y_coord].to_i
     new_rank =  piece_params[:promotion_type].to_s
-    if @piece.name == "Black_King"  && ((@piece.x_coord == 0) || (@piece.x_coord == 7)) && (@piece.y_coord == 0)
+    if @piece.promotion? == true
+      @piece.update(piece_params)
+      @piece.pawn_promote(new_rank)
+      redirect_to @game
+      flash[:notice] = 'You have successfully promoted your pawn! Please refresh the page.'
+      @game.reload
+    elsif @piece.name == "Black_King"  && ((@piece.x_coord == 0) || (@piece.x_coord == 7)) && (@piece.y_coord == 0) && ! @piece.legal_move?(x_path, y_path)
       @piece.castle(x_path, y_path)
     elsif @piece.name == "White_King" && ((@piece.x_coord == 0) || (@piece.x_coord == 7)) && (@piece.y_coord == 7)
       @piece.castle(x_path, y_path)
@@ -28,25 +34,33 @@ class PiecesController < ApplicationController
       redirect_to @game
       flash[:notice] = 'You have successfully promoted your pawn! Please refresh the page.'
       @game.reload
-    elsif piece = @piece.name == "Black_King"
+    elsif piece = @piece.name == "Black_King" 
       if @piece.black_right_castle(x_path, y_path) == true 
-        @piece.black_right_castle(x_path, y_path)
-        flash[:notice] = 'You have successfully completed Castling.'
-        @game.reload
+        unless King.legal_move?(x_path, y_path)
+          @piece.black_right_castle(x_path, y_path) 
+          flash[:notice] = 'You have successfully completed Castling.'
+          @game.reload
+        end
       else
-        @piece.white_left_castle(x_path, y_path)
-        flash[:notice] = 'You have successfully completed Castling.'
-        @game.reload
+        unless King.legal_move?(x_path, y_path)
+        @piece.white_left_castle(x_path, y_path); 
+          flash[:notice] = 'You have successfully completed Castling.'
+          @game.reload
+        end
       end
     elsif @piece.name == "White_King"
       if @piece.white_right_castle(x_path, y_path) == true 
-        @piece.white_right_castle(x_path, y_path)
-        flash[:notice] = 'You have successfully completed Castling.'
-        @game.reload
+        unless King.legal_move?(x_path, y_path)
+          @piece.white_right_castle(x_path, y_path) unless valid_move?(x_path, y_path)
+          flash[:notice] = 'You have successfully completed Castling.'
+          @game.reload
+        end
       else @piece.white_left_castle(x_path, y_path) == true
-        @piece.white_left_castle(x_path, y_path)
-        flash[:notice] = 'You have successfully completed Castling.'
-        @game.reload
+        unless King.legal_move?(x_path, y_path)
+          @piece.white_left_castle(x_path, y_path) 
+          flash[:notice] = 'You have successfully completed Castling.'
+          @game.reload
+        end
       end
     elsif @piece.valid_move?(x_path, y_path)
       @piece.move_to!(x_path, y_path)
