8c075f7fc356d8c2978f5bf47aa6ce6e4353f042
Mike Tracz <miketracz@hotmail.com>
2019-11-04 03:06:01 +0000

    rake version

--- db/migrate/20191015222933_add_initial_position_to_pieces.rb
+++
@@ -1,5 +0,0 @@
-class AddInitialPositionToPieces < ActiveRecord::Migration[5.2]
-  def change
-    add_column :pieces, :initial_position?, :boolean, :default => true
-  end
-end
--- db/migrate/20191017212905_remove_initial_postion_from_pieces.rb
+++
@@ -1,5 +0,0 @@
-class RemoveInitialPostionFromPieces < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :pieces, :initial_postion?, :boolean, :default => true
-  end
-end
